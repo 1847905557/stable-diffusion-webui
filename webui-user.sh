aac24d1f89d54b5830ce2ab1dc1385e2e75f5ea4
AnyIDElse <42762138+1847905557@users.noreply.github.com>
2023-01-30 02:14:28 +0800

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--port 6006"
+export COMMANDLINE_ARGS="--port 6006  --data-dir /root/autodl-tmp/"
 
 # python3 executable
 #python_cmd="python3"
